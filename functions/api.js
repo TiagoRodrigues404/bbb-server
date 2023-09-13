775912ad9ecca7227818b7e2cf002f53fce747ff
OlenaLiekan <olena.liekan@gmail.com>
2023-09-13 18:27:14 +0300

    Fixed: renamed app to api

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('../db');
-const app = express();
+const api = express();
 const models = require('../models/models');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
@@ -14,25 +14,25 @@ const pg = require('pg');
 
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'static')));
-app.use(fileUpload({}));
-app.use('/api', router);
+api.use(cors());
+api.use(express.json());
+api.use(express.static(path.resolve(__dirname, 'static')));
+api.use(fileUpload({}));
+api.use('/api', router);
 
 //Last in list
-app.use(errorHandler);
+api.use(errorHandler);
 
-exports.handler = serverless(app);
+exports.handler = serverless(api);
 
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-    app.listen(PORT, () => {
+    api.listen(PORT, () => {
       console.log(`App running on port ${PORT}`.bgWhite.black);
     });
-    app.get('/api/test', (req, res) => {
+    api.get('/api/test', (req, res) => {
       res.json({
         message: 'Hello from backend bbb-server express.js',
       });
