189783d92b76bfd80c298c224f989de0aae4c879
OlenaLiekan <olena.liekan@gmail.com>
2023-09-13 13:30:19 +0300

    Feature: creating netlify-lambda scripts

--- src/api.js
+++ src/api.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('./db');
+const sequelize = require('../db');
 const app = express();
-const models = require('./models/models');
+const models = require('../models/models');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
-const router = require('./routes/index');
-const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const router = require('../routes/index');
+const errorHandler = require('../middleware/ErrorHandlingMiddleware');
 const path = require('path');
 const colors = require('colors');
+const serverless = require('serverless-http');
 
 const PORT = process.env.PORT || 3001;
 
@@ -16,10 +17,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
-app.use('/api', router);
+app.use('/.netlify/functions/api', router);
 
 //Last in list
 app.use(errorHandler);
+
+module.exports = app;
+module.exports.handler = serverless(app);
 
 const start = async () => {
   try {
@@ -28,7 +32,7 @@ const start = async () => {
     app.listen(PORT, () => {
       console.log(`App running on port ${PORT}`.bgWhite.black);
     });
-    app.get('/api', (req, res) => {
+    app.get('/', (req, res) => {
       res.json({
         message: 'Hello from backend bbb-server express.js',
       });
