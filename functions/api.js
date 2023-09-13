0a74b8fdb8e7b26a399a42042e4202bef12d35de
OlenaLiekan <olena.liekan@gmail.com>
2023-09-13 16:53:40 +0300

    Feature: testing server

--- functions/api.js
+++ functions/api.js
@@ -31,7 +31,7 @@ const start = async () => {
     app.listen(PORT, () => {
       console.log(`App running on port ${PORT}`.bgWhite.black);
     });
-    app.get('/', (req, res) => {
+    app.get('/api', (req, res) => {
       res.json({
         message: 'Hello from backend bbb-server express.js',
       });
