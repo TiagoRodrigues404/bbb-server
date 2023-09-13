ba8b6cc20364e158db2b1a335f27465494b4e9b7
OlenaLiekan <olena.liekan@gmail.com>
2023-09-13 16:14:26 +0300

    Fixed: netlify deploy settings

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('./db');
+const sequelize = require('../db');
 const app = express();
-const models = require('./models/models');
+const models = require('../models/models');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
-const router = require('./routes/index');
-const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const router = require('../routes/index');
+const errorHandler = require('../middleware/ErrorHandlingMiddleware');
 const path = require('path');
 const colors = require('colors');
 const serverless = require('serverless-http');
@@ -22,7 +22,7 @@ app.use('/api', router);
 //Last in list
 app.use(errorHandler);
 
-export const handler = serverless(app);
+export const handler = serverless(api);
 
 const start = async () => {
   try {
