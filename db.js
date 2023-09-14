26332754a788810e01fa46cfeff900113def12cb
OlenaLiekan <olena.liekan@gmail.com>
2023-09-14 18:42:55 +0300

    .

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'postgres',
-  host: process.env.DB_HOST,
+  host: '127.0.0.1',
   dialectModule: pg,
   port: process.env.DB_PORT,
 });
