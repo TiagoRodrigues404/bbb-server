a61f1d65af90e027e01287420a7324934cd32ee3
OlenaLiekan <olena.liekan@gmail.com>
2023-09-15 16:48:59 +0300

    .

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 const pg = require('pg');
 require('dotenv').config();
 
-module.exports = new Sequelize('BestBuyBeauty', 'postgres', 'root', {
+module.exports = new Sequelize('postgres', 'postgres', 'root', {
   host: '127.0.0.1',
   dialect: 'postgres',
   dialectModule: pg,
