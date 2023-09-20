a0921a925be148c875c987aa818ed4dc7dcffb38
OlenaLiekan <olena.liekan@gmail.com>
2023-09-20 12:45:44 +0300

    Fixed: serverless functions for imgs

--- functions/static.js
+++ functions/static.js
@@ -9,17 +9,7 @@ const serverless = require('serverless-http');
 
 app.use(cors());
 app.use(express.json());
-app.use('/static', express.static(path.resolve(__dirname, 'static')));
+app.use('/static', express.static(path.join(process.cwd(), 'static')));
 app.use(fileUpload({}));
 
-exports.handler = async (request, context, callback) => {
-  const path2 = path.join(process.cwd(), 'static', 'catalog-08.jpg');
-  const pngBuffer = fs.readFileSync(path2);
-
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'image/jpg' },
-    body: pngBuffer.toString('base64'),
-    isBase64Encoded: true,
-  };
-};
+exports.handler = serverless(app, { binary: true });
