819a75de7ab49ab15ee87b254614680296c59288
OlenaLiekan <olena.liekan@gmail.com>
2023-09-20 17:46:54 +0300

    .

--- functions/api.js
+++ functions/api.js
@@ -15,7 +15,7 @@ const fs = require('fs');
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', express.static(path.join('/dist', 'static')));
+app.use('/api', express.static(path.join(process.cwd(), 'static')));
 app.use(fileUpload({}));
 app.use('/api', router);
 
