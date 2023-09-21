37eab3d9373561b28a977c3e950cd45671153ce2
OlenaLiekan <olena.liekan@gmail.com>
2023-09-21 16:01:00 +0300

    .

--- functions/api.js
+++ functions/api.js
@@ -20,7 +20,7 @@ app.use('/api', express.static(path.join(process.cwd(), 'static')));
 app.use(fileUpload({}));
 app.use('/api', router);
 
-/*const setSecurityHeaders = (_, res, next) => {
+const setSecurityHeaders = (_, res, next) => {
   res.set({
     'X-Content-Type-Options': 'nosniff',
     'X-Frame-Options': 'DENY',
@@ -35,10 +35,10 @@ app.use('/api', router);
     'Permissions-Policy': 'camera=(), microphone=(), geolocation=(), payment=()',
   });
   next();
-};*/
+};
 
-//app.disable('x-powered-by');
-//app.use(setSecurityHeaders);
+app.disable('x-powered-by');
+app.use(setSecurityHeaders);
 
 //Last in list
 app.use(errorHandler);
