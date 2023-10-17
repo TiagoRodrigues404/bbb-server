1a371d2e8ddc16e86fca7cb97a83cae011222dca
OlenaLiekan <olena.liekan@gmail.com>
2023-10-17 18:03:38 +0300

    .

--- routes/deliveryRouter.js
+++ routes/deliveryRouter.js
@@ -5,7 +5,7 @@ const checkRole = require('../middleware/checkRoleMiddleware');
 
 router.post('/', checkRole('ADMIN'), deliveryController.create);
 router.get('/', deliveryController.getAll);
-router.getOne('/:id', deliveryController.getOne);
+router.get('/:id', deliveryController.getOne);
 router.patch('/:id', deliveryController.update);
 
 module.exports = router;
