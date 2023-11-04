ef37c2ebe45a0e2bd537f5134eb5aa7e6c92f1da
OlenaLiekan <olena.liekan@gmail.com>
2023-11-04 23:24:00 +0200

    .

--- routes/typeRouter.js
+++ routes/typeRouter.js
@@ -6,7 +6,7 @@ const checkRole = require('../middleware/checkRoleMiddleware');
 router.post('/', checkRole('ADMIN'), typeController.create);
 router.get('/', typeController.getAll);
 router.get('/:id', typeController.getOne);
-router.delete('/', checkRole('ADMIN'), typeController.destroy);
+router.delete('/', typeController.destroy);
 router.patch('/:id', checkRole('ADMIN'), typeController.update);
 
 module.exports = router;
