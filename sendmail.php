087b70ce78c96340d547350a12a5e54eec3b3464
OlenaLiekan <olena.liekan@gmail.com>
2023-11-23 22:17:07 +0200

    .

--- sendmail.php
+++ sendmail.php
@@ -22,7 +22,7 @@ $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
 $mail->setFrom('melioraspero24@gmail.com', 'Best Buy Beauty');
 $mail->addReplyTo('melioraspero24@gmail.com', 'Best Buy Beauty');
-$mail->addAddress("olena.liekan@gmail.com","");
+$mail->addAddress('maledi2010@i.ua');
 $mail->isHTML(true);
 $mail->Subject = "PHPMailer SMTP test";
 
