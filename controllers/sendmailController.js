54be5238c8003124a8e76b1a18f8865ba3781376
OlenaLiekan <olena.liekan@gmail.com>
2023-11-24 11:32:37 +0200

    .

--- controllers/sendmailController.js
+++ controllers/sendmailController.js
@@ -4,7 +4,7 @@ class SendmailController {
   async send(req, res, next) {
     try {
       const { userName, userSurname } = req.body;
-      let result = await fetch('../sendmail.php', { userName, userSurname });
+      let result = await fetch('sendmail.php', { userName, userSurname });
       return res.json(result);
     } catch (e) {
       next(ApiError.badRequest(e.message));
