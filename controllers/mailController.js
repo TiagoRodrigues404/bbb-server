7f1f340b333644b2f82f75f773055b40f9ed109d
OlenaLiekan <olena.liekan@gmail.com>
2023-11-27 13:22:42 +0200

    .

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -3,19 +3,11 @@ const sendEmail = require('../sendEmail');
 
 class MailController {
   async send(req, res) {
-    const { email, nome } = req.body;
+    const { email, userName } = req.body;
     try {
-      sendEmail(email, nome);
-      res.render('email-form', {
-        status: 'success',
-        message: 'Email sent successfully',
-      });
+      sendEmail(email, userName);
     } catch (error) {
       console.log(error);
-      res.render('email-form', {
-        status: 'error',
-        message: 'Error sending email',
-      });
     }
   }
 }
