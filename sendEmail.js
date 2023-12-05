128e68300a25ef4a578dd2674e71037610f85960
OlenaLiekan <olena.liekan@gmail.com>
2023-12-05 14:49:38 +0200

    .

--- sendEmail.js
+++ sendEmail.js
@@ -37,7 +37,7 @@ const sendEmail = async (to, name, surname, orderNumber, address, phone, order, 
                 <p style='border-bottom: 2px solid #f6f6f6; padding: 0 0 20px 0;'>
                     Método de pagamento na sua escolha:
                 </p>
-                <div style='display: flex; flex-direction: column; border-bottom: 2px solid #f6f6f6; padding: 0 0 20px 0; text-align: left; align-items: center; justify-content: center;'>
+                <div style='border-bottom: 2px solid #f6f6f6; padding: 0 0 20px 0;'>
                     ${paymentList}
                 </div>
                 <p>
