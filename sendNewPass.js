f68dd2724aa51087d9d51c76cce8e53d8e0ea16b
Olena Liekan <olena.liekan@gmail.com>
2024-11-26 20:29:45 +0200

    .

--- sendNewPass.js
+++ sendNewPass.js
@@ -16,14 +16,20 @@ const sendNewPass = async (to, name, newPassword) => {
       },
     });
     const message = {
-      to: `maledi2010@i.ua`,
-      from: `bestbuybeauty.pt@gmail.com`,
+      to,
+      from: `Best Buy Beauty ${user}`,
       subject: `Solicitação de alteração de senha`,
       html: `
-<div>New pass</div>
+<div>${newPassword}</div>
         `,
     };
-
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Server is ready to take our messages');
+      }
+    });
     const info = await transporter.sendMail(message);
 
     console.log(('Message sent', info.messageId));
